ed289490c3b54de48c683c682b28136004a32946
riece <riece@psybergate.co.za>
2024-10-19 20:24:40 +0200

    add new catalogue tile

--- src/app/features/catalogue/components/ui/home/home.module.ts
+++ src/app/features/catalogue/components/ui/home/home.module.ts
@@ -1,17 +1,19 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CatalogueTileComponent} from './catalogue-tile/catalogue-tile.component';
-
+import {NewCatalogueTileComponent} from './new-catalogue-tile/new-catalogue-tile.component';
 
 @NgModule({
   declarations: [
-    CatalogueTileComponent
+    CatalogueTileComponent,
+    NewCatalogueTileComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    CatalogueTileComponent
+    CatalogueTileComponent,
+    NewCatalogueTileComponent
   ]
 })
 export class HomeModule {
---
+++ src/app/features/catalogue/components/ui/home/new-catalogue-tile/new-catalogue-tile.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-new-catalogue-tile',
+  templateUrl: './new-catalogue-tile.component.html',
+  styleUrl: './new-catalogue-tile.component.css'
+})
+export class NewCatalogueTileComponent {
+
+}
