696fd26203ca8cd9d9a223ef26d5c59c145dcc89
riece <riece@psybergate.co.za>
2025-05-09 15:33:56 +0200

    added visual indicator for selected Channel
    Mimics the Discord design and displays a row with the AudioService as a
    'user' in the Channel.
    
    This does duplicate the information available on the AudioOverlay,
    but it feels natural to see it there as the Aside is meant to mimic the
    Discord UI which does the same.
    
    This will also be useful behaviour for indicating whether there are any
    other ServerAudios connected to the Channel (when we eventually support
    both the 'ambience' and 'background music' AudioServices.

--- src/app/features/audio/components/audio-overlay/channel-selector/channel-selector.component.ts
+++ src/app/features/audio/components/audio-overlay/channel-selector/channel-selector.component.ts
@@ -1,7 +1,16 @@
 import {Component, Input} from '@angular/core';
 import {Channel} from "../../../../../core/audio/models/channel";
 import {SelectorComponent} from "../../../../../shared/components/selector-component";
+import {ServerAudio} from '../../../../../core/audio/models/server-audio';
+import {AudioService} from '../../../../../core/audio/models/audio-service';
 
+/**
+ * TODO [design] consider defining a Channel component that renders itself,
+ *  updating its display if it the one selected/if it has audio in it,
+ *  if it is selected etc. The list of these components can then be in the html
+ *  of the overlay rather than its own component as this is no longer a simple
+ *  list selection.
+ */
 @Component({
   selector: 'app-channel-selector',
   templateUrl: './channel-selector.component.html',
@@ -11,5 +20,17 @@ export class ChannelSelectorComponent extends SelectorComponent<Channel> {
 
   @Input({required: true})
   public channels!: Channel[];
+
+  /**
+   * TODO I may not be doing this correctly if I have to use 'null' as part of the type.
+   */
+  @Input({required: true})
+  public serverAudio?: ServerAudio|null;
+
+  /**
+   * To pull information for what icon to render.
+   */
+  @Input({required: true})
+  public audioService!: AudioService;
 
 }
