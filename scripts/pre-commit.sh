6039197d1baccbdbed76d5a04e2924dabb767401
riece <riece@psybergate.co.za>
2025-06-22 13:09:15 +0200

    added linting to pre-commit
    Commits must pass linting and keep the build green.

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -10,10 +10,16 @@ NG=ng
 # GREEN='\033[0;32m'
 # NONE='\033[0m'
 
-printf  '\n\n\033[0;33m%s\033[0m\n' "Building..."
-if ! $NG build; then
-  printf '\n\n\033[0;31m%s\033[0m\n\n' "[Unstable] Build failed!"
+printf  '\n\n\033[0;33m%s\033[0m\n' "Linting..."
+if ! $NG lint; then
+  printf '\n\n\033[0;31m%s\033[0m\n\n' "[Dirty] Lint failed!"
   exit 1
 fi
+printf  '\n\n\033[0;32m%s\033[0m\n\n' "[Clean] Lint OK."
 
-printf  '\n\n\033[0;32m%s\033[0m\n\n' "[Stable] Commit OK. (Hint: --no-verify for quick amending)"
+printf  '\n\n\033[0;33m%s\033[0m\n' "Building..."
+if ! $NG build; then
+  printf '\n\n\033[0;31m%s\033[0m\n\n' "[Red] Build failed!"
+  exit 1
+fi
+printf  '\n\n\033[0;32m%s\033[0m\n\n' "[Green] Commit OK. (Hint: --no-verify for quick amending)"
