589e420471ce72c962a4f8fdc836b6e2825dff1e
riece <riece@psybergate.co.za>
2025-06-27 12:53:45 +0200

    fix app layout
    The header, overlay, and any control bars should remain fixed on the
    viewport while the main content area scrolls. This gives the illusion of
    an application in fullscreen mode which was the intention.
    
    [revisions]
    - HomeComponent now wraps PlaylistTile articles in a section. This
      section is what we want to scroll on the Playlists view, while
      allowing the control bar to remain fixed in place.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Observable, Subject } from "rxjs";
 import { AudioService } from "./core/audio/models/audio-service";
 import { SubscriptionComponent } from "./shared/components/subscription-component";
 import { AudioRepositoryService } from "./core/audio/services/resources/audio-repository.service";
-import { AsyncPipe } from "@angular/common";
+import { AsyncPipe, NgTemplateOutlet } from "@angular/common";
 import { AudioOverlayComponent } from "./features/audio/components/audio-overlay/audio-overlay.component";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./core/layout/header/header.component";
@@ -11,7 +11,14 @@ import { HeaderComponent } from "./core/layout/header/header.component";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  imports: [AudioOverlayComponent, RouterOutlet, HeaderComponent, AsyncPipe],
+  styleUrl: "./app.component.css",
+  imports: [
+    AudioOverlayComponent,
+    RouterOutlet,
+    HeaderComponent,
+    AsyncPipe,
+    NgTemplateOutlet,
+  ],
 })
 export class AppComponent extends SubscriptionComponent implements OnInit {
   readonly #service: Subject<AudioService> = new Subject<AudioService>();
