76b411c8e24b3b7857a11a683b3645823d72b2bb
riecenaidoo <riecenaidoo@outlook.com>
2025-11-27 16:45:56 +0200

    [revision] leverage AudioStateService in AudioOverlayToggleComponent, AppComponent

--- src/app/features/audio/components/audio-overlay-toggle/audio-overlay-toggle.component.ts
+++ src/app/features/audio/components/audio-overlay-toggle/audio-overlay-toggle.component.ts
@@ -1,6 +1,17 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { DebounceDirective } from "../../../../shared/directives/debounce.directive";
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  signal,
+  Signal,
+  WritableSignal,
+} from "@angular/core";
 import { NgOptimizedImage } from "@angular/common";
+import {
+  AudioBot,
+  AudioStateService,
+} from "../../services/audio-state.service";
 
 @Component({
   selector: "app-audio-overlay-toggle",
@@ -8,59 +19,44 @@ import { NgOptimizedImage } from "@angular/common";
   templateUrl: "./audio-overlay-toggle.component.html",
   styleUrl: "./audio-overlay-toggle.component.css",
 })
-export class AudioOverlayToggleComponent extends DebounceDirective {
-  #initialToggleState = false;
+export class AudioOverlayToggleComponent {
+  // ==========================================================================
+  // Dependencies
+  // ==========================================================================
+
+  readonly #bot: AudioBot = inject(AudioStateService);
+
+  // ==========================================================================
+  // State
+  // ==========================================================================
+
+  protected readonly connected: Signal<boolean> = computed(
+    () => this.#bot.audioBot() !== undefined,
+  );
+
+  protected readonly reconnecting: WritableSignal<boolean> = signal(false);
+
+  // ==========================================================================
+  // Initialisation
+  // ==========================================================================
 
   /**
-   * The current toggle state.
-   *
-   * @private
+   * @remarks When we trigger a {@link AudioBot#reconnect} we set out loading state ({@link reconnecting}). This must be
+   * synced with the {@link AudioBot} state, so when an update occurs we clear our loading state.
    */
-  #toggle = false;
-
-  // ------ API ------
-
-  /**
-   * The User has requested to toggle the `AudioService` on/off.
-   *
-   * If an `initialToggle` was provided, it will only emit when the User toggles to the other state.
-   * The User input is debounced. The delay is configurable with `debounceDelayMs`.
-   *
-   * @type {EventEmitter<void>}
-   * @protected
-   * @see initialToggle
-   * @see DebounceDirective.debounceDelayMs
-   */
-  @Output()
-  protected toggled: EventEmitter<void> = new EventEmitter<void>();
-
-  /**
-   * Set the initial state of the toggle. By default, it is `false` (i.e. off).
-   *
-   * A `toggled` event will only be fired when the User toggles to the other state.
-   *
-   * @param {boolean} isToggled
-   */
-  @Input({ required: false })
-  public set initialToggle(isToggled: boolean) {
-    this.#initialToggleState = isToggled;
-    this.#toggle = isToggled;
-  }
-
-  // ------ Component ------
-
-  protected toggledOn(): boolean {
-    return this.#toggle;
-  }
-
-  // ------ Event Handling ------
-
-  protected toggle(): void {
-    this.#toggle = !this.#toggle;
-    this.debounce(() => {
-      if (this.#toggle !== this.#initialToggleState) {
-        this.toggled.emit();
-      }
+  public constructor() {
+    effect(() => {
+      this.#bot.audioBot();
+      this.reconnecting.set(false);
     });
+  }
+
+  // ==========================================================================
+  // Event Handling
+  // ==========================================================================
+
+  protected reconnect(): void {
+    this.#bot.reconnect();
+    this.reconnecting.set(true);
   }
 }
