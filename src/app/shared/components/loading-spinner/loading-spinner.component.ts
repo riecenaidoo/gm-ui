0790322d193eeca9e39fdbfb34974cc1d44b8bab
riecenaidoo <riecenaidoo@outlook.com>
2026-09-24 21:01:12 +0200

    documentation

--- src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -1,5 +1,14 @@
 import { Component, input, InputSignal } from "@angular/core";
 
+/**
+ * Project content into the component to block it with a loading spinner, while something is {@link #loading}.
+ *
+ * ```html
+ *   <app-loading-spinner [loading]="isConnecting()">
+ *     <button (click)="disconnect()">Disconnect</button>
+ *   </app-loading-spinner>
+ * ```
+ */
 @Component({
   selector: "app-loading-spinner",
   imports: [],
@@ -11,5 +20,10 @@ export class LoadingSpinnerComponent {
   // API
   // ==========================================================================
 
+  /**
+   * Render the loading spinner while `true`, and the projected content while `false`. Defaults to `true`.
+   *
+   * @see {@link debounced}
+   */
   public readonly loading: InputSignal<boolean> = input(true);
 }
